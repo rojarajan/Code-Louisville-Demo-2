a9348d416f1be076d44f7cb4fc440f119154653f
Prem Raj <rojarajan@gmail.com>
2022-06-09 12:19:34 -0400

    delete blanks

--- KnowledgeCheck.py
+++ KnowledgeCheck.py
@@ -2,7 +2,6 @@ a = "Hello World"
 print (a)
 b = ['abc', 23, 2.45, 'louisville']
 print(b[3])
-
 d = {"code1" : 901, "code2" : 902, "code3" : 903}
 print(d["code2"])
 t = (20, 30, "R4","ru8", 0.235)
