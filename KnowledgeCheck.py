298900704143dcdba13b311d064c19f9dc18db9d
Prem Raj <rojarajan@gmail.com>
2022-06-09 12:18:00 -0400

    changes

--- KnowledgeCheck.py
+++ KnowledgeCheck.py
@@ -3,9 +3,7 @@ print (a)
 b = ['abc', 23, 2.45, 'louisville']
 print(b[3])
 
-d = {"code1" : 901, "code2": 902}
-for key, value in d.items():
-    print(key)
-    print(value)
-t = (20, 30, "R4")
-print(t[1])
+d = {"code1" : 901, "code2" : 902, "code3" : 903}
+print(d["code2"])
+t = (20, 30, "R4","ru8", 0.235)
+print(t[2])
