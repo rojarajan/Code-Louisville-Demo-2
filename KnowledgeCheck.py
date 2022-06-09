d7c536db770cb527fbbdc41cd9709690e42c6512
Prem Raj <rojarajan@gmail.com>
2022-06-09 11:59:12 -0400

    add list and dict

--- KnowledgeCheck.py
+++ KnowledgeCheck.py
@@ -1,2 +1,11 @@
 a = "Hello World"
 print (a)
+b = ['abc', 23, 2.45, 'louisville']
+print(b[3])
+
+d = {"code1" : 901, "code2": 902}
+for key, value in d.items():
+    print(key)
+    print(value)
+t = (20, 30, "R4")
+print(t[1])
